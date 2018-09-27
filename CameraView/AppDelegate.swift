59813cf9ee95a85aa39c30816df25e104c30af98
Mozzart <timokhovim@gmail.com>
2018-09-27 12:01:42 +0300

    Initial Commit

---
+++ CameraView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CameraView
+//
+//  Created by Ivan Timokhov on 27/09/2018.
+//  Copyright © 2018 Ivan Timokhov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
