d1c34d042136b6f570f4c88a9cfc69499df67346
Mozzart <timokhovim@gmail.com>
2018-09-28 17:49:01 +0300

    Context menu with preset sizes created

--- CameraView/ViewController.swift
+++ CameraView/ViewController.swift
@@ -8,6 +8,7 @@
 
 import Cocoa
 import AVFoundation
+import CoreBluetooth
 
 class ViewController: NSViewController {
 
@@ -51,6 +52,8 @@ class ViewController: NSViewController {
         // Start session
         self.captureSession.startRunning()
         
+        
+        
     }
     
     //MARK:- Set Window properties
@@ -59,12 +62,15 @@ class ViewController: NSViewController {
         view.window?.level = .floating
         // Make window visible on all spaces
         view.window?.collectionBehavior = .canJoinAllSpaces
+        // Set window aspect ratio
+        view.window?.aspectRatio = NSSize(width: 16, height: 9)
 
         view.window?.titleVisibility = .visible
         view.window?.title = "Camera View"
         view.window?.styleMask.insert(.fullSizeContentView)
     }
     
+    //MARK:- Create capture session
     func setupCaptureSession() {
         self.captureSession = AVCaptureSession()
     }
@@ -85,33 +91,93 @@ class ViewController: NSViewController {
         }
     }
     
+    //MARK:- Create context menu
     override func rightMouseUp(with event: NSEvent) {
         let menu = NSMenu()
-        let menuItem480 = NSMenuItem.init(title: "480x270", action: #selector(setWindowSize), keyEquivalent: "")
-        menu.addItem(menuItem480)
-//        menu.item
+        for item in presetSizesArray {
+            let menuItem = NSMenuItem(title: item.title, action: #selector(self.setWindowSize), keyEquivalent: "")
+            menu.addItem(menuItem)
+        }
         NSMenu.popUpContextMenu(menu, with: event, for: self.previewView)
-        
     }
     
+    //Mark:- Preset of window size
+    enum presetSizes: String {
+        case fullScreen = "Full Screen"
+        case halfScreen = "1/2"
+        case quoterScreen = "1/4"
+        case octalScreen = "1/8"
+        case sixteenthScreen = "1/16"
+    }
+    let presetSizesArray: Array<menuItem> = [
+        menuItem(title: "Full Screen", value: presetSizes.fullScreen),
+        menuItem(title: "1/2", value: presetSizes.halfScreen),
+        menuItem(title: "1/4", value: presetSizes.quoterScreen),
+        menuItem(title: "1/8", value: presetSizes.octalScreen),
+        menuItem(title: "1/16", value: presetSizes.sixteenthScreen),
+    ]
     @objc func setWindowSize(_ menuItem: NSMenuItem) {
-        
-        switch menuItem.title {
-        case "480x270":
-            var windowFrame = view.window?.frame
-            windowFrame?.size = NSMakeSize(420, 220)
-            
-            self.view.window?.setFrame(windowFrame!, display: true)
-
-        default:
-            print("Ooops: \(menuItem.title)")
+        guard let presetSize = presetSizesArray[presetSizesArray.index(where: {$0.title == menuItem.title})!].value as? presetSizes else {
+            print("Unsetted value")
+            return
+        }
+        guard let screenHeight = self.view.window?.screen?.frame.height else {
+            fatalError("Cant take screen height")
+        }
+        guard let screenWidth = self.view.window?.screen?.frame.width else {
+            fatalError("Cant take screen width")
+        }
+        guard var windowFrame = view.window?.frame else {
+            fatalError("No window frame")
         }
         
+        var height: CGFloat
+        var width: CGFloat!
+        var xPoint: CGFloat!
+        var yPoint: CGFloat!
+        
+
+        switch presetSize {
+        case .fullScreen:
+            width = screenWidth
+            xPoint = 0.0
+            yPoint = 0.0
+
+        case .halfScreen:
+            width = screenWidth / 2
+            
+        case .quoterScreen:
+            width = screenWidth / 4
+        case .octalScreen:
+            width = screenWidth / 8
+        case .sixteenthScreen:
+            width = screenWidth / 16
+        }
+
+        height = width / 16 * 9
+
+        if windowFrame.origin.x + width > screenWidth {
+            xPoint = screenWidth - width
+        } else {
+            xPoint = windowFrame.origin.x
+        }
+        if windowFrame.origin.y + height > screenHeight {
+            yPoint = screenHeight - height
+        } else {
+            yPoint = windowFrame.origin.y
+        }
+
+        windowFrame.size = NSMakeSize(width, height)
+        windowFrame.origin = NSMakePoint(xPoint, yPoint)
+
+        self.view.window?.setFrame(windowFrame, display: true)
+        self.view.setFrameSize(NSSize(width: width, height: height))
+        self.view.setBoundsSize(NSSize(width: width, height: height))
     }
-    
-    //TODO:- Make preset of window size
-    
-    //TODO:- Make save window proportions on resize
-    
+
+    struct menuItem {
+        var title: String
+        var value: Any
+    }
 }
 
